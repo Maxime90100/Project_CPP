20509384093746a75eef563c05e04314c6231155
maxime <maxime.theveneau@gmail.com>
2023-03-13 12:21:54 +0100

    set main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,19 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QString appTitle = "My TODO List";
+
+    setWindowTitle(appTitle);
+    QMessageBox::information(this, tr("Project C++ IUTBM S4"), tr("Welcome to %1 !\nCreated by THEVENEAU Maxime and PERRIN Antoine").arg(appTitle));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
