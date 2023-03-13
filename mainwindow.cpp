89d76902f6bbf0952c04f5ae81a8d995007fd328
maxime <maxime.theveneau@gmail.com>
2023-03-13 14:27:00 +0100

    add button connect

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,8 @@
 #include "ui_mainwindow.h"
 #include <QMessageBox>
 
+QVariant _addButton;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -12,7 +14,11 @@ MainWindow::MainWindow(QWidget *parent)
     setWindowTitle(appTitle);
     ui->appTitle->setText(appTitle);
     ui->todoAddTitle->setText("Add a Todo");
-    ui->todoButton->setText("Add");
+
+    _addButton.setValue(ui->todoButton);
+    QPushButton* button = ui->todoButton;
+    button->setText("Add");
+    connect(button, &QPushButton::clicked, this, &MainWindow::button_clicked);
 
     QMessageBox::information(this, tr("Project C++ IUTBM S4"), tr("Welcome to %1 !\nCreated by THEVENEAU Maxime and PERRIN Antoine").arg(appTitle));
 }
@@ -20,4 +26,9 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::button_clicked(){
+    QPushButton* button = _addButton.value<QPushButton*>();
+    button->setText("Todo has been correctly added !");
 }
