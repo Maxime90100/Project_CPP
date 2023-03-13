ba242e85c8a45dfc3cfa4a8663deb2b24fbf1b5e
maxime <maxime.theveneau@gmail.com>
2023-03-13 14:57:50 +0100

    add addTodo check

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,7 +18,6 @@ MainWindow::MainWindow(QWidget *parent)
     ui->todoAddTitle->setText("Add a Todo");
 
     _addButton->setText("Add");
-    connect(_addButton, &QPushButton::clicked, this, &MainWindow::addTodo);
 
     QMessageBox::information(this, tr("Project C++ IUTBM S4"), tr("Welcome to %1 !\nCreated by THEVENEAU Maxime and PERRIN Antoine").arg(appTitle));
 }
@@ -30,8 +29,19 @@ MainWindow::~MainWindow()
 
 void MainWindow::init(){
     _addButton = ui->todoButton;
+    connect(_addButton, &QPushButton::clicked, this, &MainWindow::addTodo);
 }
 
 void MainWindow::addTodo(){
-    _addButton->setText("Todo has been correctly added !");
+    QLineEdit* title = ui->todoTitle;
+    QPlainTextEdit* text = ui->todoText;
+    QDateEdit* date = ui->todoDate;
+
+    if (title->text().isEmpty() || text->toPlainText().isEmpty() || date->text().isEmpty()) {
+        _addButton->setText("You must complete all informations !");
+    }
+    else {
+        _addButton->setText("Todo has been correctly added !");
+    }
 }
+
