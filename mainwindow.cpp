0b315384487880700c076e08bf378d4ba7340953
maxime <maxime.theveneau@gmail.com>
2023-03-13 12:33:37 +0100

    add button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,6 +10,7 @@ MainWindow::MainWindow(QWidget *parent)
     QString appTitle = "My TODO List";
 
     setWindowTitle(appTitle);
+    //ui->appTitle.setText(appTitle);
     QMessageBox::information(this, tr("Project C++ IUTBM S4"), tr("Welcome to %1 !\nCreated by THEVENEAU Maxime and PERRIN Antoine").arg(appTitle));
 }
 
